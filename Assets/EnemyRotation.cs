9d53c973ed1b58cdb7d911b48306d7081ad5a02b
Ovrdsg <leeeeenny01@gmail.com>
2022-02-08 16:08:02 +0300

    ИИ

---
+++ Assets/EnemyRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyRotation : MonoBehaviour
+{
+    public AIPath aIPath;
+
+    
+    void Update()
+    {
+        if(aIPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        else if(aIPath.desiredVelocity.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
